10e4c22ec12ebf99f7efc4a266940cace4e90187
김준철 <jckim@sbcn.co.kr>
2020-02-08 12:36:06 +0900

    Lab01 코드 추가
    Lab01 예제 코드를 추가함.

---
+++ lab01/src/main/java/net/jetalab/spreinglab01/SpReIngLab01Application.java
@@ -0,0 +1,13 @@
+package net.jetalab.spreinglab01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpReIngLab01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpReIngLab01Application.class, args);
+	}
+
+}
